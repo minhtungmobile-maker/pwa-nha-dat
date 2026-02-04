570b7ce4c51c3547ce6848c53dda8a1403181659
minhtungmobile-maker <minhtungmobile@gmail.com>
2026-02-04 19:12:05 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,18 @@
-﻿const CACHE_NAME = "hstai777-pwa-v1";
-const ASSETS = [
+﻿const CACHE_NAME = "pwa-nha-dat-v1";
+const FILES = [
   "/",
   "/index.html",
-  "/manifest.json",
-  "/assets/css/style.css",
-  "/assets/js/app.js"
+  "/manifest.json"
 ];
 
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(c => c.addAll(FILES))
   );
 });
 
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
